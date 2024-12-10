59b796b02eba8cf48c43f06f10e059e91560a3e2
Eduardo <dudu_piccioni@hotmail.com>
2024-12-09 22:53:01 -0300

    modified:   api/models/index.js

--- api/models/index.js
+++ api/models/index.js
@@ -4,15 +4,14 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-<<<<<<< HEAD
-<<<<<<< HEAD
 const getConfig = require("../../config/database.js");
 
 const db = {};
 
-const initializeModels = async () => {  
+const initializeModels = async () => {
   const resolvedConfig = await getConfig();
   const sequelize = new Sequelize(resolvedConfig);
+
   try {
     const files = await fs.promises.readdir(__dirname);
 
@@ -40,39 +39,3 @@ const initializeModels = async () => {
 };
 
 module.exports = initializeModels;
-=======
-=======
->>>>>>> 83649143cc91188e0f3a7534ff54971c594552c4
-const config = require("../../config/database.js");
-
-const db = {};
-const sequelize = new Sequelize(config);
-
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
-<<<<<<< HEAD
->>>>>>> 8364914 (Primeiro commit no branch pr-cicd)
-=======
->>>>>>> 83649143cc91188e0f3a7534ff54971c594552c4
